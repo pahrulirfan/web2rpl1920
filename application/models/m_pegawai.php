22299475f9595dc62d948c35dc128e65e4d2184d
Pahrul Irfan <ip.5090@gmail.com>
2019-10-15 08:25:35 +0800

    2 cara di model

--- application/models/m_pegawai.php
+++ application/models/m_pegawai.php
@@ -3,12 +3,13 @@
 class M_pegawai extends CI_Model
 {
 //    cara 1 dengan membuat property
-    protected $table = 'tbl_031';
+//    protected $table = 'tbl_031';
 
     public function semua()
     {
 //        cara 1 mengambil property tabel
 //        return $this->db->get($this->table)->result();
+
 //        cara 2 dengan langsung menulis nama tabel
         return $this->db->get('tbl_031')->result();
     }
