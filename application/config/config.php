c0cac18e82e62d399628216cd2d5ad06e6c85587
Pahrul Irfan <ip.5090@gmail.com>
2019-11-06 09:50:53 +0800

    routes, config

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
-*/
-$config['base_url'] = 'http://localhost/ci/';
+//*/
+$config['base_url'] = 'http://localhost/CI/';
 
 /*
 |--------------------------------------------------------------------------
