7cfe6226c49a5ff388effa2af685085aac57ed74
Pahrul Irfan <ip.5090@gmail.com>
2019-11-07 11:05:39 +0800

    keterangan akses

--- application/controllers/Sinopsis.php
+++ application/controllers/Sinopsis.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sinopsis extends CI_Controller {
 
+	// akses lewat web browser sinopsis/yogi
 	public function yogi()
 	{
 		echo "Nama : Yoghy Pratama";
